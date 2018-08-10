65a250d2ee9a0948e4ac8b67ffbac277aaec1984
YongHoonJJo <starctak@gmail.com>
2018-08-11 01:08:04 +0900

    2-03 Show, Edit, Update, Destroy

--- contact-book/index.js
+++ contact-book/index.js
@@ -1,9 +1,26 @@
 var express = require("express");
 var mongoose = require("mongoose");
-var bodyParser = require("body-parser"); // 1
+var bodyParser = require("body-parser"); 
+var methodOverride = require("method-override"); // 1 
 var app = express();
 
-// DB setting ...
+   
+// DB Setting...
+
+// 환경변수에 저장된 값을 사용하여 mongoDB에 접속
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true });
+var db = mongoose.connection; // mongoose의 db object를 가져와 db변수에 대입
+
+// db가 성공적으로 연결된 경우 "DB connected"를 출력
+db.once("open", function(){
+ console.log("DB connected");
+});
+
+// db연결중 에러가 있는 경우 "DB ERROR : " 과 에러를 출력
+db.on("error", function(err){
+ console.log("DB ERROR : ", err);
+});
+
 
 // Other settings
 app.set("view engine", "ejs");
@@ -14,6 +31,12 @@ app.use(express.static(__dirname+"/public"));
 // 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
 app.use(bodyParser.json()); // 2
 app.use(bodyParser.urlencoded({extended:true})); // 3
+
+// _method의 query로 들어오는 값으로 HTTP method를 바꿉니다. 
+// 예를들어 http://example.com/category/id?_method=delete를 받으면 
+/// _method의 값인 delete을 읽어 해당 request의 HTTP method를 delete으로 바꿉니다.
+app.use(methodOverride("_method")); 
+
 
 // DB schema // 4
 // mongoose.Schema 함수를 사용해서 schema object를 생성
@@ -85,18 +108,61 @@ app.post("/contacts", function(req, res){
 });
 
 
-// 환경변수에 저장된 값을 사용하여 mongoDB에 접속
-mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true });
-var db = mongoose.connection; // mongoose의 db object를 가져와 db변수에 대입
-
-// db가 성공적으로 연결된 경우 "DB connected"를 출력
-db.once("open", function(){
- console.log("DB connected");
+// Contacts - show // 3 
+// "contacts/:id"에 get 요청이 오는 경우 :
+// :id처럼 route에 콜론(:)을 사용하면 해당 위치의 값을 받아 req.params에 넣게 됩니다.
+// 예를 들어 "contacts/abcd1234"가 입력되면 "contacts/:id" route에서 이를 받아 
+// req.params.id에 "abcd1234"를 넣게 됩니다.
+app.get("/contacts/:id", function(req, res){
+    Contact.findOne({_id:req.params.id}, function(err, contact){
+     if(err) return res.json(err);
+     res.render("contacts/show", {contact:contact});
+    });
 });
 
-// db연결중 에러가 있는 경우 "DB ERROR : " 과 에러를 출력
-db.on("error", function(err){
- console.log("DB ERROR : ", err);
+// Model.findOne은 DB에서 해당 model의 document를 하나 찾는 함수
+// 첫번째 parameter로 찾을 조건을 object로 입력하고 data를 찾은 후 callback함수를 호출합니다. 
+// Model.find와 비교하면, Model.find는 조건에 맞는 결과를 모두 찾아 array로 전달하는 반면,
+// Model.findOne은 조건에 맞는 결과를 하나 찾아 object로 전달합니다. (검색 결과가 없다면 null이 전달됩니다.)
+// 위 경우에는 {_id:req.params.id}를 조건으로 전달하고 있는데, 즉 DB의 contacts collection에서 _id가 req.params.id와 일치하는 data를 찾는 조건입니다.
+// 에러가 없다면 검색 결과를 받아 views/contacts/show.ejs를 render합니다.
+   
+
+// Contacts - edit // 4 
+// "contacts/:id/edit"에 get 요청이 오는 경우 :
+// Model.findOne이 다시 사용되었습니다. 검색 결과를 받아 views/contacts/edit.ejs를 render합니다.
+app.get("/contacts/:id/edit", function(req, res){
+    Contact.findOne({_id:req.params.id}, function(err, contact){
+     if(err) return res.json(err);
+     res.render("contacts/edit", {contact:contact});
+    });
+});
+
+// Contacts - update // 5 
+// "contacts/:id"에 put 요청이 오는 경우 :
+// Model.findOneAndUpdate는 DB에서 해당 model의 document를 하나 찾아 그 data를 수정하는 함수
+// 첫번째 parameter로 찾을 조건을 object로 입력하고,
+// 두번째 parameter로 update 할 정보를 object로 입력data를 찾은 후 callback함수를 호출합니다.
+// 이때 callback함수로 넘겨지는 값은 수정되기 전의 값입니다. 
+// 만약 업데이트 된 후의 값을 보고 싶다면 callback 함수 전에 parameter로 {new:true}를 넣어주면 됩니다.
+// Data 수정 후 "/contacts/"+req.params.id로 redirect합니다.
+app.put("/contacts/:id", function(req, res){
+    Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+     if(err) return res.json(err);
+     res.redirect("/contacts/"+req.params.id);
+    });
+});
+
+// Contacts - destroy // 6
+// "contacts/:id"에 delete 요청이 오는 경우 :
+// Model.remove는 DB에서 해당 model의 document를 하나 찾아 삭제하는 함수
+// 첫번째 parameter로 찾을 조건을 object로 입력하고 data를 찾은 후 callback함수를 호출합니다.
+// Data 삭제후 "/contacts"로 redirect 함.
+app.delete("/contacts/:id", function(req, res){
+    Contact.remove({_id:req.params.id}, function(err, contact){
+     if(err) return res.json(err);
+     res.redirect("/contacts");
+    });
 });
 
 // Port setting
